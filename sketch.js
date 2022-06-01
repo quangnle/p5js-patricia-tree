b7a20d8e4a222845ccc953c329deffbe54c2c295
Quang Le <mnest2409@yahoo.com>
2022-06-01 10:39:35 +0700

    added some test nodes

--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
 let t = new PatriciaTree(0,30,600);
-t.addNode('abcd');
-t.addNode('abcd1');
-t.addNode('abcd2');
-t.addNode('abcd3');
-t.addNode('abcd4');
-t.addNode('abcd5');
-t.addNode('abcd6');
-t.addNode('abcd7');
-t.addNode('abcd8');
-t.addNode('bef');
-t.addNode('beg');
-t.addNode('bek');
-t.addNode('c');
+let nodes = ['A',
+    'B',
+    'BC',
+    'BE',
+    'BF',
+    'BEF',
+    'BEK',
+    'ABCD',
+    'ABCD1',
+    'ABCD2',
+    'ABCD3',
+    'ABCD4',
+    'ABCD5',
+    'ABCD6',
+    'ABCD7'];
+
+for	(let i=0; i < nodes.length; i++){
+	t.addNode(nodes[i]);
+}
 
 function setup() {
 	createCanvas(600, 400);
